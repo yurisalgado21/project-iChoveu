d45112f1ca7ac19a3c4325272a96f69e4b1f3711
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-27 16:47:53 -0300

    Exercício 1 feito

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,18 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
+export const searchCities = async (term) => {
 //   seu código aqui
+  const API_CITIES = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+  try {
+    const CITY = await fetch(API_CITIES);
+    const data = await CITY.json();
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
